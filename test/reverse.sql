67cd1d5d0551b24822414d60de89eb7d48bb1f65
Roman Heinrich <roman.heinrich@gmail.com>
2021-10-14 18:09:55 +0200

    Fix failing test

--- test/reverse.sql
+++ test/reverse.sql
@@ -3,5 +3,5 @@
 select '01', reverse(null) is NULL;
 select '02', reverse('hello') = 'olleh';
 select '03', reverse('привет') = 'тевирп';
-select '04', reverse("𐌀𐌁𐌂") = '𐌂𐌁𐌀';
+select '04', reverse('𐌀𐌁𐌂') = '𐌂𐌁𐌀';
 select '05', reverse('hello 42@ world') = 'dlrow @24 olleh';
